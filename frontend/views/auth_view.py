24c8b92c96f5a3b44c66211081a67d1a4d6953fb
Pavan Badempet <pavan9b@gmail.com>
2025-10-09 12:49:32 +0530

    fix: Format SVG as single line to prevent code block rendering

--- frontend/views/auth_view.py
+++ frontend/views/auth_view.py
@@ -102,12 +102,13 @@ div[data-testid="stFormSubmitButton"] button:active {
 """, unsafe_allow_html=True)
 
     # Custom Header with SVG Logo
-    svg_logo = """
-    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="#2563EB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/>
-        <path d="M12 5 9.04 11H6a1 1 0 0 0 0 2h4.37l1.63-3.26L14.6 15h3.83a1 1 0 0 0 0-2h-3.04l-1.63-3.26z"/>
-    </svg>
-    """
+    # Flattened to single line to prevent Markdown interpreting indentation as code blocks
+    svg_logo = (
+        '<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="#2563EB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">'
+        '<path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/>'
+        '<path d="M12 5 9.04 11H6a1 1 0 0 0 0 2h4.37l1.63-3.26L14.6 15h3.83a1 1 0 0 0 0-2h-3.04l-1.63-3.26z"/>'
+        '</svg>'
+    )
     
     col1, col2 = st.columns([1.2, 1])
     
