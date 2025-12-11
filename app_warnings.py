6ac1032f3ca6422788cd78fba5f25bea32cfa196
Pavan Badempet <pavan9b@gmail.com>
2025-12-11 21:48:53 +0530

    Add Render Config

--- app_warnings.py
+++
@@ -1,3 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module="langchain_core._api.deprecation")
-warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
