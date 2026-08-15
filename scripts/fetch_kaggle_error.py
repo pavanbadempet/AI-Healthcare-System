46c9fc31c71f9149b2a9435ed8a9d1ba02d102fd
Pavan Badempet <pavan9b@gmail.com>
2026-08-16 00:45:54 +0530

    chore: remove redundant repomix dump and obsolete temporary debug scripts

--- scripts/fetch_kaggle_error.py
+++
@@ -1,7 +0,0 @@
-import requests
-import os
-url = "https://dbc-3f46f628-dd14.cloud.databricks.com/api/2.1/jobs/runs/export?run_id=800186599180777"
-token = os.environ.get("DATABRICKS_TOKEN")
-h = {"Authorization": f"Bearer {token}"}
-r = requests.get(url, headers=h)
-print(r.json().get("views", [{}])[0].get("content", "No content"))
