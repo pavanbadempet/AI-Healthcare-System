dd919f479bf90ea8d678f7345e557efac0044ebc
Pavan Badempet <pavan9b@gmail.com>
2026-08-16 20:19:45 +0530

    chore(core): remove orphaned boilerplate stubs and consolidate core backend architecture

--- backend/sota_algorithms.py
+++
@@ -1,8 +0,0 @@
-"""
-Backward-compatibility bridge for backend.data_structures algorithms
-"""
-from backend.data_structures import ConsistentHashRing, HyperLogLogCounter, MinHashDeduplicator
-
-hll_counter = HyperLogLogCounter()
-minhash_dedup = MinHashDeduplicator()
-hash_ring = ConsistentHashRing()
--- backend/sota_data_structures.py
+++
@@ -1,7 +0,0 @@
-"""
-Backward-compatibility bridge for backend.data_structures
-"""
-from backend.data_structures import BloomFilter, CircularRingBuffer, RadixPrefixTrie, RadixPrefixTrieNode
-
-bloom_filter = BloomFilter()
-radix_trie = RadixPrefixTrie()
